75919b0b2f5e16de5c2e805915f37758533e7aa3
Andres Lopez Gomez <andreslg20@gmail.com>
2025-08-15 19:46:36 -0500

    Update config.php

--- panel/config.php
+++ panel/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$url = 'https://mysistemaweb.com/diagnostika';
+//$url = 'https://mysistemaweb.com/diagnostika';
 
-//$url = 'https://diagnostika:8890';
+$url = 'https://diagnostika:8890';
 
 $empresa = 'Diagnostika Diesel Global';
 $titulo = 'Panel de Administracion - ' . $empresa;
