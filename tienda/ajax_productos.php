02f41f47b254ff5111928a71a6441bcc86264310
Andres Lopez Gomez <andreslg20@gmail.com>
2025-08-19 16:38:20 -0500

    .

--- tienda/ajax_productos.php
+++ tienda/ajax_productos.php
@@ -208,7 +208,7 @@ try {
 
     // Campos para el frontend
     $prod['imagen_url']  = $url.'/'.$imagen_principal ?: '';
-    $prod['gallery']     = $galeria_full;              // array para data-gallery
+    $prod['gallery']     = $url.'/'.$galeria_full;              // array para data-gallery
     $prod['marca']       = $prod['marca'] ?? '';       // data-brand
     $prod['descripcion'] = $prod['descripcion'] ?? ''; // data-desc
   }
